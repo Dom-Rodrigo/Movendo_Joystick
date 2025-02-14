161101b9f302945415225780aeaa3e7f2453f56c
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-02-14 02:27:08 -0300

    Botao A desliga/liga leds_pwm

--- Movendo_joystick.c
+++ Movendo_joystick.c
@@ -10,10 +10,12 @@
 #define LED_BLUE 12
 #define LED_RED 13
 #define BUTTON_JOYSTICK 22
+#define BUTTON_A 5
 #define BUTTON_BOOTSEL 6
 
 bool red_on = false;
 bool blue_on = false;
+bool leds_pwm = true;
 
 uint32_t last_time;
 void gpio_irq_handler(uint gpio, uint32_t event_mask) {
@@ -25,6 +27,10 @@ void gpio_irq_handler(uint gpio, uint32_t event_mask) {
         }
         if (!gpio_get(BUTTON_JOYSTICK)) {
             gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+            last_time = current_time;
+        }
+        if (!gpio_get(BUTTON_A)) {
+            leds_pwm = !leds_pwm;
             last_time = current_time;
         }
     }   
@@ -40,38 +46,7 @@ uint pwm_setup(uint LED){
     return slice;
 }
 
-
-int main()
-{
-    stdio_init_all();
-    adc_init();
-    uint slice_red = pwm_setup(LED_RED);
-    uint slice_blue = pwm_setup(LED_BLUE);
-
-    adc_gpio_init(27);
-    adc_gpio_init(26);
-    
-    gpio_init(LED_GREEN);
-    gpio_set_dir(LED_GREEN, GPIO_OUT);
-
-    gpio_init(BUTTON_JOYSTICK);
-    gpio_set_dir(BUTTON_JOYSTICK, GPIO_IN);
-    gpio_pull_up(BUTTON_JOYSTICK);
-
-    gpio_init(BUTTON_BOOTSEL);
-    gpio_set_dir(BUTTON_BOOTSEL, GPIO_IN);
-    gpio_pull_up(BUTTON_BOOTSEL);
-
-    gpio_set_irq_enabled_with_callback(BUTTON_BOOTSEL, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    gpio_set_irq_enabled_with_callback(BUTTON_JOYSTICK, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
-    while (true) {
-        adc_select_input(0);
-        uint16_t vry_value = adc_read();
-
-        adc_select_input(1);
-        uint16_t vrx_value = adc_read();
-        
+void handle_leds_joystick_pwm(uint16_t vrx_value, uint16_t vry_value){
         if (vry_value < 2087+600 && vry_value > 2087-600) // VALOR DE Y CONTROLA LED AZUL, APAGA SE TIVER NO MEIO
             blue_on = false;
         else 
@@ -91,7 +66,47 @@ int main()
             pwm_set_gpio_level(LED_RED, vrx_value);
         else
             pwm_set_gpio_level(LED_RED, 0);
+}
 
+int main()
+{
+    stdio_init_all();
+    adc_init();
+    uint slice_red = pwm_setup(LED_RED);
+    uint slice_blue = pwm_setup(LED_BLUE);
+
+    adc_gpio_init(27);
+    adc_gpio_init(26);
+    
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+
+    gpio_init(BUTTON_JOYSTICK);
+    gpio_set_dir(BUTTON_JOYSTICK, GPIO_IN);
+    gpio_pull_up(BUTTON_JOYSTICK);
+
+    gpio_init(BUTTON_BOOTSEL);
+    gpio_set_dir(BUTTON_BOOTSEL, GPIO_IN);
+    gpio_pull_up(BUTTON_BOOTSEL);
+
+    gpio_set_irq_enabled_with_callback(BUTTON_BOOTSEL, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_JOYSTICK, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    while (true) {
+        adc_select_input(0);
+        uint16_t vry_value = adc_read();
+
+        adc_select_input(1);
+        uint16_t vrx_value = adc_read();
+        
+        if (leds_pwm){
+            handle_leds_joystick_pwm(vrx_value, vry_value);
+        }
 
         printf("X: %d\n", vrx_value);
         printf("Y: %d\n", vry_value);
