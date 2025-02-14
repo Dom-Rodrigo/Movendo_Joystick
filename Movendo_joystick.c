9d2a82afcacdab2604e728bd6838cc75099625cc
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-02-14 01:34:53 -0300

    Acende leds sem pwm de acordo posicao do joystick

--- Movendo_joystick.c
+++ Movendo_joystick.c
@@ -5,7 +5,12 @@
 
 #define VRX 27 // input 1 s:24 h:1885 b:4090
 #define VRY 26 // input 0 s:24 h:2087 b:4090
+#define LED_BLUE 12
+#define LED_RED 13
 #define BUTTON_BOOTSEL 6
+
+bool red_on = false;
+bool blue_on = false;
 
 uint32_t last_time;
 void gpio_irq_handler(uint gpio, uint32_t event_mask) {
@@ -26,6 +31,12 @@ int main()
     adc_gpio_init(27);
     adc_gpio_init(26);
 
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+
     gpio_init(BUTTON_BOOTSEL);
     gpio_set_dir(BUTTON_BOOTSEL, GPIO_IN);
     gpio_pull_up(BUTTON_BOOTSEL);
@@ -37,6 +48,29 @@ int main()
 
         adc_select_input(1);
         uint16_t vrx_value = adc_read();
+        
+        if (vry_value < 2087+500 && vry_value > 2087-500)
+            blue_on = false;
+        else 
+            blue_on = true;
+    
+        if (vrx_value < 1880+500 && vrx_value > 1880-500)
+            red_on = false;
+        else
+            red_on = true;        
+        
+        if (blue_on)
+            gpio_put(LED_BLUE, 1);
+        else
+            gpio_put(LED_BLUE, 0);
+
+                
+        if (red_on)
+            gpio_put(LED_RED, 1);
+        else
+            gpio_put(LED_RED, 0);
+
+
 
         printf("X: %d\n", vrx_value);
         printf("Y: %d\n", vry_value);
